ea6ecce91050c641ec3dad949d5690fa0c52b27d
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-24 14:31:42 -0300

    feat: adciona funções de cripto e descriptografa de location search

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ function init() {
     setTimeout(() => {
         changeCard();
     }, 3000)
+
+    if(isName()){
+        let name = getName();
+    }
 }
 
 function changeCard() {
@@ -12,3 +16,38 @@ function changeCard() {
     document.getElementById('text_card').innerHTML = 'Seu nome foi encontrado clique no botão ok para ver seu cartão natalino';
 }
 
+function isName() {
+    if (location.search.length > 1)
+        return true;
+    return false;
+}
+
+function getName() {
+    let text = location.search;
+    text = text.replace('?=', '');
+
+    return Decrypt(text)
+}
+
+function Encrypt(value)  {
+  var result="";
+  for(i=0;i<value.length;i++) {
+    if(i<value.length-1) {
+        result+=value.charCodeAt(i)+10;
+        result+="-";
+    } else {
+        result+=value.charCodeAt(i)+10;
+    }
+  }
+  return result;
+}
+
+function Decrypt(value) {
+  var result="";
+  var array = value.split("-");
+
+  for(i=0;i<array.length;i++) {
+    result+=String.fromCharCode(array[i]-10);
+  }
+  return result;
+} 
